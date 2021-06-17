04a77e45d7341a628d3da4f361ddf0bc995d5dc0
M Kavya <73734974+MKAVYA01@users.noreply.github.com>
2021-06-17 17:27:07 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,15 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+int max(int x[], int y) {
+  int larger = 0;
+  for(int i = 0;i < y; i++){
+    if(x[i] > x[i+1]) {
+      larger = x[i];
+      x[i] = x[i+1];
+      x[i+1] = larger;
+    }
+  }
+  return x[y];
+}
+
